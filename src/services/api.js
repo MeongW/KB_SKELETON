1b9b1afb6553db10d69b02297849f87045c1f6fd
MeongW <125392666+MeongW@users.noreply.github.com>
2025-04-11 01:45:40 +0900

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "",
+  baseURL: "https://kb-json-server.glitch.me",
   headers: {
     "Content-Type": "application/json",
   },
