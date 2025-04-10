11faa1fb56744ab858340bf98c6e24ec2e7143b6
MeongW <125392666+MeongW@users.noreply.github.com>
2025-04-11 00:18:20 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,22 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
+// https://vite.dev/config
 export default defineConfig({
   base: "/kb-skeleton/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://kb-json-server.glitch.me",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/kb-skeleton/api/, ""),
+      },
     },
   },
   css: {
